1fb2eadfc29294e3a9be13c03a70e768bd770964
Satish Chitimoju <satishchjavastream@gmail.com>
2023-08-16 17:32:18 -0500

    spring boot java 17 application (.jar) file deployed in to EC2 instance

---
+++ src/main/java/com/selfaws/springbootawsexample/SpringBootAwsExampleApplication.java
@@ -0,0 +1,21 @@
+package com.selfaws.springbootawsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAwsExampleApplication {
+
+	@GetMapping("/")
+	public String home(){
+		return "Welcome to AWS First Deployment....!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsExampleApplication.class, args);
+	}
+
+}
